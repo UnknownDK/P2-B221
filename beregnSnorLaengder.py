b7cb56572e2bd4994f4cd882d5c033f59226de23
Bjørn Grevenkop-Castenskiold <33236231+UnknownDK@users.noreply.github.com>
2020-03-18 14:32:06 +0100

    Lavede eksempel på snorlængde beregning
    Lavede eksempel på snorlængde beregning med vektorer ved hjælp af numpy i python

---
+++ beregnSnorLaengder.py
@@ -0,0 +1,30 @@
+import numpy as np #numpy er et matematik bibliotek
+
+#Grundplanets hjørnepunkter defineres
+A = np.array([0,0,0])
+B = np.array([200,0,0])
+C = np.array([0,200,0])
+D = np.array([200,200,0])
+
+#Probepunktets koordinater indtastes af brugeren
+x = int(input("x-koordinat: "))
+y = int(input("y-koordinat: "))
+z = int(input("z-koordinat: "))
+
+#Probepunkt defineres
+Q = np.array([x,y,z])
+
+#snorvektorerne findes
+aq = Q - A
+bq = Q - B
+cq = Q - C
+dq = Q - D
+
+#Længderne af snorvektorerne findes
+aqLen = np.sqrt(aq[0]**2 + aq[1]**2 + aq[2]**2)
+bqLen = np.sqrt(bq[0]**2 + bq[1]**2 + bq[2]**2)
+cqLen = np.sqrt(cq[0]**2 + cq[1]**2 + cq[2]**2)
+dqLen = np.sqrt(dq[0]**2 + dq[1]**2 + dq[2]**2)
+
+#Længderne af snorvektorerne printes
+print("aq: {}, bq: {}, cq: {}, dq: {}".format(aqLen, bqLen, cqLen, dqLen))
